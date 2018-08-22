c3d02a77e344b3247f8cc1cb68498bcaeeb89761
Laurene <laurene.picandet@gmail.com>
2018-08-22 15:12:59 +0200

    added function to find min and max in a set

--- ensembles_devoirs.py
+++ ensembles_devoirs.py
@@ -19,3 +19,24 @@ def uniSet(s1,s2):
 
 print(uniSet(set1,set2))
 
+
+#Écrire un programme qui renvoie le minimum et le maximum d’un ensemble.
+
+#attention ! les fonctions max et min ne marchent pas sur un mélange d'entiers et de lettres
+
+
+def MinMax(s):
+    l = list(s)  # conversion de l'ensemble en liste pour avoir des indexs
+    for i in range(len(l)):  # pourquoi ici ne faut-il pas mettre "len(l)-1 ?
+        l[i] = str(l[i])  # conversion de tous les éléments en strings pour
+                           # pouvoir les comparer selon l ordre ASCII
+
+        # print("i",i)
+        # print('l[i]', l[i])
+        # print(type(l[i]))
+    # print(l)
+
+    return (min(l), max(l))
+
+
+print(MinMax(set1))
