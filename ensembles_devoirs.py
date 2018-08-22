54fea0249a11dc14de10d36f5609014ca150ae55
Laurene <laurene.picandet@gmail.com>
2018-08-22 14:18:18 +0200

    added function to find intersection of 2 sets

---
+++ ensembles_devoirs.py
@@ -0,0 +1,14 @@
+
+#Écrire un programme qui renvoie l’intersection de 2 ensembles.
+
+
+set1 = {1,2,3,'a',4,5}
+
+set2 = {'a','b',12,3}
+
+def interSet(s1,s2):
+    return(s1 &s2)
+
+print(interSet(set1,set2))
+
+
